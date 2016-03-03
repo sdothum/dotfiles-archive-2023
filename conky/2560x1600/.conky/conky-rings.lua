1da9667c82c760d0c7d52912aeb46669e557a85f
sdothum <sdothum@gmail.com>
2016-03-03 10:32:22 -0500

    herbstluftwm panel title highlight (underline or contrast only)

--- conky/2560x1600/.conky/conky-rings.lua
+++ conky/2560x1600/.conky/conky-rings.lua
@@ -9,9 +9,9 @@
 
 require 'cairo'
 
-default_color=0x8fc5d5
-color0=0x6b939f
-color6=0x28363b
+default_color=0x215e54
+color0=0x18463f
+color6=0x091a18
 
 --------------------------------------------------------------------------------
 --                                                                    clock DATA
