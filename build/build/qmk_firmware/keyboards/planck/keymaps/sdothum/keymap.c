d5cd0e5c17eda27f2b17e46022858e1f81b64835
sdothum <sdothum@gmail.com>
2016-08-24 09:26:11 -0400

    further planck navigation layer tweaks, #defines

--- build/build/qmk_firmware/keyboards/planck/keymaps/sdothum/keymap.c
+++ build/build/qmk_firmware/keyboards/planck/keymaps/sdothum/keymap.c
@@ -128,7 +128,7 @@ enum tap_dance {
 // adjust layer key
 #define ADJUST MO    (_ADJUST)
 
-#define Xparent KC_TRNS
+#define ___x___ KC_TRNS
 #define _______ KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -223,7 +223,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_ADJUST] = {
   {_______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {Xparent, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, COLEMAK,  QWERTY, DVORAK,  PLOVER,  _______},
+  {___x___, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, COLEMAK,  QWERTY, DVORAK,  PLOVER,  _______},
   {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END },
 },
@@ -245,14 +245,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_F10,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F9  },
   {KC_F11,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, Lprn,    KC_RPRN, KC_F8  },
   {KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   Lbrc,    KC_RBRC, Lcbr,    KC_RCBR, KC_F7  },
-  {Xparent, Xparent, Xparent, Xparent, Xparent, Xparent, Xparent, Xparent, KC_HOME, KC_PGDN, KC_PGUP, KC_END },
+  {___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, KC_HOME, KC_PGDN, KC_PGUP, KC_END },
 },
 
 [_SYMBOL] = {
   {KC_F10,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F9  },
   {KC_F11,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, Lprn,    KC_RPRN, KC_F8  },
   {KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   Lbrc,    KC_RBRC, Lcbr,    KC_RCBR, KC_F7  },
-  {Xparent, Xparent, Xparent, Xparent, Xparent, Xparent, Xparent, Xparent, KC_HOME, KC_PGDN, KC_PGUP, KC_END },
+  {___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, KC_HOME, KC_PGDN, KC_PGUP, KC_END },
 },
 
 // .......................................................... Hexadecimal Keypad
@@ -269,9 +269,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_KEYPAD] = {
   {_______, _______, _______, _______, _______, S(KC_E), S(KC_F), KC_7,    KC_8,    KC_9,    KC_MINS, _______},
-  {ADJUST,  _______, _______, _______, _______, S(KC_C), S(KC_D), KC_4,    KC_5,    KC_6,    KC_PLUS, Xparent},
+  {ADJUST,  _______, _______, _______, _______, S(KC_C), S(KC_D), KC_4,    KC_5,    KC_6,    KC_PLUS, ___x___},
   {_______, _______, _______, _______, _______, S(KC_A), S(KC_B), KC_1,    KC_2,    KC_3,    KC_EQL,  _______},
-  {Xparent, _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_0,    KC_DOT,  KC_SLSH, KC_ASTR, _______},
+  {___x___, _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_0,    KC_DOT,  KC_SLSH, KC_ASTR, _______},
 },
 
 // ............ ..................................................Navigation Pad
@@ -290,7 +290,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, _______, _______, _______, KC_BSPC, KC_HOME, KC_UP,   KC_END,  _______, _______},
   {_______, _______, KC_LGUI, KC_LCTL, KC_LSFT, _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______},
-  {_______, _______, _______, _______, Xparent, _______, _______, Xparent, _______, _______, _______, _______},
+  {_______, _______, _______, _______, ___x___, _______, _______, ___x___, _______, _______, _______, _______},
 },
 
 };
