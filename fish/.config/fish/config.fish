eb74262c4c39a7dc3aae3a681966064cd99e21c7
sdothum <sdothum@gmail.com>
2017-02-27 12:22:18 -0500

    rg replacement of ack, fzf fuzzy jump

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -66,7 +66,7 @@ set -x LESS '-RX -P ?B %f  %lt-%lb/%L  %Pb\%: [pipe]  %lt-%lb/\.\.'
 set -x PAGER 'less'
 
 # fzf
-set -x FZF_DEFAULT_OPTS '--reverse --border --prompt="    ─────  " --height=15 --ansi'
+set -x FZF_DEFAULT_OPTS '--reverse --border --prompt="    ─────  " --height=20 --ansi'
 set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 
 # ..................................................... Development environments
--- fish/.config/fish/functions/j.fish
+++ fish/.config/fish/functions/j.fish
@@ -1,18 +1,19 @@
 function j --description 'fuzzy jump'
+  set filter '.deprecated|.hg'
   switch (count $argv)
     case 0
-      find -type d 2>/dev/null | grep -v '.deprecated' | fzf | read dir
+      find -type d 2>/dev/null | egrep -v $filter | fzf | read dir
     case 1
       if test -d $argv[1]
-        find $argv[1] -type d 2>/dev/null | grep -v '.deprecated' | fzf | read dir
+        find $argv[1] -type d 2>/dev/null | egrep -v $filter | fzf | read dir
       else
-        find $HOME -type d 2>/dev/null | grep -v '.deprecated' | fzf -q $argv[1] | read dir
+        find $HOME -type d 2>/dev/null | egrep -v $filter | fzf -q $argv[1] | read dir
       end
     case 2
       if test -d $argv[1]
-        find $argv[1] -type d 2>/dev/null | grep -v '.deprecated' | fzf -q $argv[2] | read dir
+        find $argv[1] -type d 2>/dev/null | egrep -v $filter | fzf -q $argv[2] | read dir
       else
-        find $HOME -type d 2>/dev/null | grep -v '.deprecated' | grep "$argv[1]" | fzf -q $argv[2] | read dir
+        find $HOME -type d 2>/dev/null | egrep -v $filter | egrep "$argv[1]" | fzf -q $argv[2] | read dir
       end
   end
   cd $dir
