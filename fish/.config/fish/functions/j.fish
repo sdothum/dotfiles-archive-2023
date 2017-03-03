4a7173a95ce78d2b68ed33efa4994daf61bdaec9
sdothum <sdothum@gmail.com>
2017-03-03 16:30:19 -0500

    dynamic gui/ncurses dmenu

--- fish/.config/fish/functions/j.fish
+++ fish/.config/fish/functions/j.fish
@@ -7,13 +7,17 @@ function j --description 'fuzzy jump'
         find -type d ^/dev/null | filter | fzf | read dir
       end
     case 1
-      if test -d $argv[1]
+      if test -L $argv[1]
+        find -L $argv[1] -type d ^/dev/null | filter | fzf | read dir
+      else if test -d $argv[1]
         find $argv[1] -type d ^/dev/null | filter | fzf | read dir
       else
         find $HOME -type d ^/dev/null | filter | fzf -q $argv[1] | read dir
       end
     case 2
-      if test -d $argv[1]
+      if test -L $argv[1]
+        find -L $argv[1] -type d ^/dev/null | filter | fzf -q $argv[2] | read dir
+      else if test -d $argv[1]
         find $argv[1] -type d ^/dev/null | filter | fzf -q $argv[2] | read dir
       else
         find $HOME -type d ^/dev/null | filter | egrep "$argv[1]" | fzf -q $argv[2] | read dir
