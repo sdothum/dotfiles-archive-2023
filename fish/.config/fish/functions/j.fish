9b2eebbd3549ca650ded488e941182a04fb3e1f0
sdothum <sdothum@gmail.com>
2017-03-13 17:55:01 -0400

    implement dynamic cache for fuzzy jump (especially on arm)

--- fish/.config/fish/functions/j.fish
+++ fish/.config/fish/functions/j.fish
@@ -1,27 +1,24 @@
 function j --description 'fuzzy jump'
   switch (count $argv)
+    # use dynamic cache to speed up subsequent matches (especially arm)
     case 0
       if pwd | grep -q $HOME
-        find $HOME -type d ^/dev/null | filter | fzf | read dir
+        fcache $HOME | fzf | read dir
       else
-        find -type d ^/dev/null | filter | fzf | read dir
+        fcache $PWD | fzf | read dir
       end
     case 1
-      if test -L $argv[1]
-        find -L $argv[1] -type d ^/dev/null | filter | fzf | read dir
-      else if test -d $argv[1]
-        find $argv[1] -type d ^/dev/null | filter | fzf | read dir
+      if test -d $argv[1]
+        fcache $argv[1] | fzf | read dir
       else
-        find $HOME -type d ^/dev/null | filter | fzf -q $argv[1] | read dir
+        fcache $HOME | fzf -q $argv[1] | read dir
       end
     case 2
-      if test -L $argv[1]
-        find -L $argv[1] -type d ^/dev/null | filter | fzf -q $argv[2] | read dir
-      else if test -d $argv[1]
-        find $argv[1] -type d ^/dev/null | filter | fzf -q $argv[2] | read dir
+      if test -d $argv[1]
+        fcache $argv[1] | fzf -q $argv[2] | read dir
       else
-        find $HOME -type d ^/dev/null | filter | egrep "$argv[1]" | fzf -q $argv[2] | read dir
+        fcache $HOME | egrep "$argv[1]" | fzf -q $argv[2] | read dir
       end
   end
-  cd $dir
+  test -z $dir ;or cd $dir
 end
