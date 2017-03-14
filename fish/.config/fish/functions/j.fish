7fbb59a246c9a2063feab6c5ee855c5d67d20830
sdothum <sdothum@gmail.com>
2017-03-13 20:45:20 -0400

    fuzzy search caching

--- fish/.config/fish/functions/j.fish
+++ fish/.config/fish/functions/j.fish
@@ -3,21 +3,21 @@ function j --description 'fuzzy jump'
     # use dynamic cache to speed up subsequent matches (especially arm)
     case 0
       if pwd | grep -q $HOME
-        fcache $HOME | fzf | read dir
+        fcache d $HOME | fzf | read dir
       else
-        fcache $PWD | fzf | read dir
+        fcache d $PWD | fzf | read dir
       end
     case 1
       if test -d $argv[1]
-        fcache $argv[1] | fzf | read dir
+        fcache d $argv[1] | fzf | read dir
       else
-        fcache $HOME | fzf -q $argv[1] | read dir
+        fcache d $HOME | fzf -q $argv[1] | read dir
       end
     case 2
       if test -d $argv[1]
-        fcache $argv[1] | fzf -q $argv[2] | read dir
+        fcache d $argv[1] | fzf -q $argv[2] | read dir
       else
-        fcache $HOME | egrep "$argv[1]" | fzf -q $argv[2] | read dir
+        fcache d $HOME | egrep "$argv[1]" | fzf -q $argv[2] | read dir
       end
   end
   test -z $dir ;or cd $dir
