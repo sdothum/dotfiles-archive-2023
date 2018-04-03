aadaab70ec3db9e6b95bd627ebcb98915c90206f
sdothum <sdothum@gmail.com>
2018-04-03 10:24:16 -0400

    beakl mash up shift layer home blocks

--- planck/qmk_firmware/keyboards/planck/keymaps/sdothum/beakl.h
+++ planck/qmk_firmware/keyboards/planck/keymaps/sdothum/beakl.h
@@ -516,9 +516,9 @@
   },
 
   // ,-----------------------------------------------------------------------------------.
-  // |   Q  |   $  |   {  |   }  |   Z  | Shift| Caps |   G  |   D  |   N  |   M  |   X  |
+  // |   Q  |   Y  |   [  |   ]  |   Z  | Shift| Caps |   G  |   D  |   N  |   M  |   X  |
   // |------+------+------+------+------+-------------+------+------+------+------+------|
-  // |   K  |   /  |   <  |   >  |   ~  |Cursor| Mouse|   C  |   T  |   R  |   S  |   W  |
+  // |   K  |   ^  |   <  |   >  |   ~  |Cursor| Mouse|   C  |   T  |   R  |   S  |   W  |
   // |------+------+------+------+------+------|------+------+------+------+------+------|
   // |   J  |   :  |   !  |   =  |   `  |  Num | Regex|   B  |   P  |   L  |   F  |   V  |
   // |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -526,14 +526,14 @@
   // `-----------------------------------------------------------------------------------'
 
   [_LSHIFT] = {
-    {S(KC_Q), KC_DLR,  KC_LCBR, KC_RCBR, S(KC_Z), CNTR_TL, CNTR_TR, S(KC_G), S(KC_D), S(KC_N), S(KC_M), S(KC_X)},
-    {S(KC_K), KC_SLSH, TD_LT,   TD_GT,   TD_TILD, CNTR_HL, CNTR_HR, S(KC_C), S(KC_T), S(KC_R), S(KC_S), S(KC_W)},
+    {S(KC_Q), S(KC_Y), KC_LBRC, KC_RBRC, S(KC_Z), CNTR_TL, CNTR_TR, S(KC_G), S(KC_D), S(KC_N), S(KC_M), S(KC_X)},
+    {S(KC_K), KC_CIRC, TD_LT,   TD_GT,   TD_TILD, CNTR_HL, CNTR_HR, S(KC_C), S(KC_T), S(KC_R), S(KC_S), S(KC_W)},
     {S(KC_Z), KC_COLN, KC_EXLM, KC_EQL,  TD_GRV,  CNTR_BL, CNTR_BR, S(KC_B), S(KC_P), S(KC_L), S(KC_F), S(KC_V)},
     {OS_CTL,  OS_GUI,  OS_ALT,  S_TAB,   ___fn__, LT_INS,  S_LEFT,  KC_UNDS, SP_DEL,  S_DOWN,  S_UP,    S_RGHT },
   },
 
   // ,-----------------------------------------------------------------------------------.
-  // |   Q  |   Y  |   O  |   U  |   Z  | Shift| Caps |   G  |   [  |   ]  |   ^  |   X  |
+  // |   Q  |   Y  |   O  |   U  |   Z  | Shift| Caps |   G  |   {  |   }  |   M  |   X  |
   // |------+------+------+------+------+-------------+------+------+------+------+------|
   // |   K  |   H  |   E  |   A  |   ?  |Cursor| Mouse|   |  |   (  |   )  |   $  |   W  |
   // |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -543,10 +543,9 @@
   // `-----------------------------------------------------------------------------------'
 
   [_RSHIFT] = {
-    {S(KC_Q), S(KC_Y), S(KC_O), S(KC_U), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_G), KC_LBRC, KC_RBRC, KC_CIRC, S(KC_X)},
+    {S(KC_Q), S(KC_Y), S(KC_O), S(KC_U), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_G), KC_LCBR, KC_RCBR, S(KC_M), S(KC_X)},
     {S(KC_K), S(KC_H), S(KC_E), S(KC_A), KC_QUES, CNTR_HL, CNTR_HR, KC_PIPE, KC_LPRN, KC_RPRN, KC_DLR,  S(KC_W)},
     {S(KC_J), TD_COLN, TD_DQOT, S(KC_I), KC_SLSH, CNTR_BL, CNTR_BR, KC_BSLS, KC_ASTR, KC_DOT,  S(KC_P), S(KC_V)},
     {OS_CTL,  OS_GUI,  OS_ALT,  KC_TAB,  KC_MINS, LT_INS,  S_LEFT,  ___fn__, SP_BSPC, S_DOWN,  S_UP,    S_RGHT },
   },
 #endif
-
