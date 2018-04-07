41e5cba0d7be4aee356863d23275c72f9ecbb81d
sdothum <sdothum@gmail.com>
2018-04-07 13:35:58 -0400

    planck and splitography common library

--- splitography/qmk_firmware/keyboards/splitography/keymaps/beakl8/keymap.c
+++ splitography/qmk_firmware/keyboards/splitography/keymaps/beakl8/keymap.c
@@ -61,9 +61,9 @@
 
 
 
-//            === N O T E ===
+//                === N O T E ===
 //
-// export CPATH=<keymap.c directory>/common
+// sudo CPATH=<keymap.c directory>/common make ...
 
 
 #include "config.h"
@@ -335,7 +335,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     case LT_BSPC:
       tap_layer(record, _SYMBOL);
       // LT (_SYMBOL, KC_LEFT) left right combination layer
-      tri_layer(record, RIGHT, 0, 0, _SYMBOL, _LSHIFT, _MOUSE);
+      to_and_fro(record, RIGHT, 0, 0, _SYMBOL, _LSHIFT);
       break;
     case OS_ALT:
       tap_mods(record, KC_LALT);
@@ -369,12 +369,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     case LT_BSLS:
       tap_layer(record, _MOUSE);
       // LT (_MOUSE, KC_BSLS) left right combination layer, see #define LT_BSLS
-      tri_layer(record, LEFT, 0, 0, _MOUSE, _SYMBOL, _MOUSE);
+      to_and_fro(record, LEFT, 0, 0, _MOUSE, _SYMBOL);
       break;
     case SL_DEL:
       tap_layer(record, _MOUSE);
       // LT (_MOUSE, S(KC_DEL)) left right combination layer
-      tri_layer(record, RIGHT, NOSHIFT, KC_DEL, _MOUSE, _LSHIFT, _MOUSE);
+      to_and_fro(record, RIGHT, NOSHIFT, KC_DEL, _MOUSE, _LSHIFT);
       break;
     case SL_PIPE:
       // LT (_EDIT, S(KC_BSLS)) emulation
@@ -391,7 +391,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     case TD_SPC:
       tap_layer(record, _LSHIFT);
       // LT (_LSHIFT, KC_SPC) left right combination layer, see tap dance TD_SPC
-      tri_layer(record, LEFT, 0, 0, _LSHIFT, _SYMBOL, _MOUSE);
+      to_and_fro(record, LEFT, 0, 0, _LSHIFT, _SYMBOL);
       break;
 #ifdef CENTER_TT
     case CNTR_TL:
