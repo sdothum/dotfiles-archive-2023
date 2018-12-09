05398bb68901314308e6251063982f0406d4eab0
Steven Hum <sdothum@gmail.com>
2018-12-09 07:53:32 -0500

    beakl fix release timing on map_shift

--- splitography/qmk_firmware/keyboards/splitography/keymaps/beaklvi/common/.deprecated/keycode_functions.c
+++ splitography/qmk_firmware/keyboards/splitography/keymaps/beaklvi/common/.deprecated/keycode_functions.c
@@ -116,7 +116,7 @@ bool map_shift(keyrecord_t *record, uint16_t shift_key, uint8_t shift, uint16_t 
     }
     else {
       unregister_code(keycode);
-      if (!shift) { register_code(KC_LSFT); }    // restore SFT_T
+      // if (!shift) { register_code(KC_LSFT); } // omit restore and potential (pre-release) SFT_T timing lock
     }
     return true;                                 // remap complete, see process_record_user()
   }
