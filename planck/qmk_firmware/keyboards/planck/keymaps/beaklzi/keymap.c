2cd0cdd5623a53068b563f59e40e366968d7c14a
Steven Hum <sdothum@gmail.com>
2020-07-25 10:01:36 -0400

    comments

--- planck/qmk_firmware/keyboards/planck/keymaps/beaklzi/keymap.c
+++ planck/qmk_firmware/keyboards/planck/keymaps/beaklzi/keymap.c
@@ -117,7 +117,7 @@ enum keyboard_keycodes {
  ,PLOVER
 #endif
 #ifdef ROLLOVER
- ,HOME_Q  // pseudo GUI_T(KC_A)
+ ,HOME_Q  // pseudo GUI_T(KC_Q)
  ,HOME_H  // pseudo CTL_T(KC_H)
  ,HOME_E  // pseudo ALT_T(KC_E)
  ,HOME_A  // pseudo SFT_T(KC_A)
