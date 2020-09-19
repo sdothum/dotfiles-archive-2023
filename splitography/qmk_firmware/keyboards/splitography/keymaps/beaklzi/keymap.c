58503a74826488fb63329c157dc0086247f1c297
Steven Hum <sdothum@gmail.com>
2020-09-19 11:32:29 -0400

    #defines

--- splitography/qmk_firmware/keyboards/splitography/keymaps/beaklzi/keymap.c
+++ splitography/qmk_firmware/keyboards/splitography/keymaps/beaklzi/keymap.c
@@ -223,7 +223,7 @@ static uint16_t td_timer = 0;  // pseudo tapdance timer
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
-  if (reshifted && !mod_down(KC_LSFT)) { unregister_code(KC_LSFT); reshifted = 0; }  // see map_shift()
+  if (reshifted && !MOD_DOWN(KC_LSFT)) { unregister_code(KC_LSFT); reshifted = 0; }  // see map_shift()
 
   // ........................................................ Home Row Modifiers
 
@@ -250,9 +250,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 #else
   case HOME_A:
     leadercap = KEY_DOWN ? 1 : 0;  // space/enter + shift shortcut, see leader_cap()
-    mod_bits(record, KC_LSFT);                          break;
+    MOD_BITS(record, KC_LSFT);                          break;
   case HOME_T:
-    mod_bits(record, KC_RSFT);                          break;
+    MOD_BITS(record, KC_RSFT);                          break;
 #endif
 
   // ............................................................. Toggle Layers
@@ -401,7 +401,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     if (map_leader(record, LEFT, KC_RSFT, NOSHIFT, KC_COLN, 4)) { return false; }
     break;
   case TD_COLN:
-    if (mod_down(KC_RSFT))                                      { unregister_code(KC_RSFT); }  // *must* un-shift before tap dance processing to register unshifted keycodes
+    if (MOD_DOWN(KC_RSFT))                                      { unregister_code(KC_RSFT); }  // *must* un-shift before tap dance processing to register unshifted keycodes
     leadercap = KEY_DOWN ? 1 : 0;  // semi/colon + space/enter + shift shortcut, see leader_cap()
     set_leader(record, LEFT, KC_RSFT, NOSHIFT, KC_COLN, 4);
     break;
@@ -424,7 +424,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     if (map_shift(record, KC_RSFT, NOSHIFT, KC_COLN))           { return false; }
     break;
   case TD_COLN:
-    if (mod_down(KC_RSFT))                                      { unregister_code(KC_RSFT); }  // *must* un-shift before tap dance processing to register unshifted keycodes
+    if (MOD_DOWN(KC_RSFT))                                      { unregister_code(KC_RSFT); }  // *must* un-shift before tap dance processing to register unshifted keycodes
     leadercap = KEY_DOWN ? 1 : 0;  // semi/colon + space/enter + shift shortcut, see leader_cap()
     break;
 
