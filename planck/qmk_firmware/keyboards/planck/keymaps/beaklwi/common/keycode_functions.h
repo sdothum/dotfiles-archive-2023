ba19bf453b8687207604b0a8a73c724f96d54ba4
Steven Hum <sdothum@gmail.com>
2021-02-02 09:40:35 -0500

    map_shift rolling_mod corner case, code compression via macros, better function naming

--- planck/qmk_firmware/keyboards/planck/keymaps/beaklwi/common/keycode_functions.h
+++ planck/qmk_firmware/keyboards/planck/keymaps/beaklwi/common/keycode_functions.h
@@ -5,14 +5,13 @@
 bool key_press(RECORD);
 void layer_toggle(RECORD, uint8_t layer, bool upcase, uint16_t keycode);
 bool leader_cap(RECORD, uint8_t layer, uint16_t keycode);
-bool map_leader(RECORD, uint16_t sftcode, bool upcase, uint16_t keycode, uint8_t column);
 bool map_shift(RECORD, uint16_t sftcode, bool upcase, uint16_t keycode);
-bool map_shifted(RECORD, uint16_t sftcode, bool upcase, uint16_t keycode, uint8_t layer);
+bool map_shift_event(RECORD, uint16_t sftcode, bool upcase, uint16_t keycode, uint8_t column);
 bool mod_roll(RECORD, uint16_t modifier, bool upcase, uint16_t keycode, uint8_t column);
 void mod_tap(RECORD, uint16_t modifier, bool upcase, uint16_t keycode);
 bool raise_layer(RECORD, uint8_t layer, uint8_t side, bool invert);
 void rolling_layer(RECORD, uint8_t side, bool upcase, uint16_t keycode, uint8_t layer, uint8_t facing);
-void set_leader(RECORD, uint16_t keycode, uint8_t column);
+void set_leadercap(RECORD, uint16_t keycode, uint8_t column);
 void steno(RECORD);
 void toggle(RECORD, uint16_t modifier, uint16_t keycode);
 void tt_escape(RECORD, uint16_t keycode);
