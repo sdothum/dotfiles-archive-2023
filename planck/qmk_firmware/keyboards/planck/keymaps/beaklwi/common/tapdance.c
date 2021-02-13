6131de8cc6a3bc46d0e5d1cadb89ec5056b36e92
Steven Hum <sdothum@gmail.com>
2021-02-13 09:55:07 -0500

    _delay_ms() replaced with wait_ms(), stagger set to 2 (wi-x)

--- planck/qmk_firmware/keyboards/planck/keymaps/beaklwi/common/tapdance.c
+++ planck/qmk_firmware/keyboards/planck/keymaps/beaklwi/common/tapdance.c
@@ -119,7 +119,7 @@ void dot_reset(STATE, void *user_data)
 
 // ............................................................... Paste Actions
 
-#define IRC_ENTER _delay_ms(10); TAP(KC_ENT)
+#define IRC_ENTER wait_ms(10); TAP(KC_ENT)
 
 #define CTL_V     register_code(KC_LCTL); TAP      (KC_V); unregister_code(KC_LCTL)
 #define CTL_SFT_V register_code(KC_LCTL); TAP_SHIFT(KC_V); unregister_code(KC_LCTL)
