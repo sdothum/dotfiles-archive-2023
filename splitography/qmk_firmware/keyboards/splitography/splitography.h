31be3e04001876502da6b4cdafbcc1d68957a38f
Steven Hum <sdothum@gmail.com>
2021-05-07 11:52:52 -0400

    splito chording engine

--- splitography/qmk_firmware/keyboards/splitography/splitography.h
+++ splitography/qmk_firmware/keyboards/splitography/splitography.h
@@ -23,16 +23,29 @@
 // The second converts the arguments into a two-dimensional array
 
 #define KEYMAP( \
-	k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, \
-	k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, \
-	k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, \
-	k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b  \
-) \
-{ \
+	k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b,     \
+	k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b,     \
+	k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b,     \
+	k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b      \
+)                                                                 \
+{                                                                 \
 	{ k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b }, \
 	{ k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b }, \
 	{ k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b }, \
 	{ k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b }  \
+}
+
+#define LAYOUT_splitography(                                                      \
+	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B,                     \
+	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B,                     \
+	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,                     \
+	                    K34, K35, K36, K37                                          \
+)                                                                                 \
+{                                                                                 \
+	{ K00,   K01,   K02,   K03,   K04, K05, K06, K07, K08,   K09,   K0A,   K0B   }, \
+	{ K10,   K11,   K12,   K13,   K14, K15, K16, K17, K18,   K19,   K1A,   K1B   }, \
+	{ K20,   K21,   K22,   K23,   K24, K25, K26, K27, K28,   K29,   K2A,   K2B   }, \
+	{ KC_NO, KC_NO, KC_NO, KC_NO, K34, K35, K36, K37, KC_NO, KC_NO, KC_NO, KC_NO }  \
 }
 
 #endif
